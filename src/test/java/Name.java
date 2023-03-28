e8b9b36118a09731593e1d4a3483ec37b14665b7
lnoskov <k.noskov.kazanexpress@gmail.com>
2023-03-28 14:31:46 +0300

    Ex3 and Ex4

--- src/test/java/Name.java
+++ src/test/java/Name.java
@@ -4,6 +4,6 @@ import org.junit.jupiter.api.Test;
 public class Name {
     @Test
     public void MyName(){
-        System.out.println("Hello from Leonid");
+        System.out.println("Hello from Леонид");
     }
 }
