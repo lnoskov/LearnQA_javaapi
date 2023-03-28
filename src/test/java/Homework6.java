4c85bad924d76334d54ba5cb6fb0115816a3269a
lnoskov <k.noskov.kazanexpress@gmail.com>
2023-03-28 17:45:00 +0300

    Ex6

---
+++ src/test/java/Homework6.java
@@ -0,0 +1,21 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Homework6 {
+    @Test
+    public void testRestAssured(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        response.prettyPrint();
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+
+    }
+}
