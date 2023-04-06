44235890648f5213a11e6f837341418d1af21674
lnoskov <k.noskov.kazanexpress@gmail.com>
2023-04-07 01:31:57 +0300

    Ex13

---
+++ src/test/java/Homework13.java
@@ -0,0 +1,32 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class Homework13 {
+    @ParameterizedTest
+    @CsvSource({"'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',Mobile,No,Android",
+            "'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',Mobile,Chrome,iOS",
+            "'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',Googlebot,Unknown,Unknown",
+            "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0',Web,Chrome,No",
+            "'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',Mobile,No,iPhone"})
+
+    public void userAgent(String userAgent, String platforms, String browsers, String devices){
+        JsonPath response = RestAssured
+                .given()
+                .header("User-agent", userAgent)
+                .when()
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+        String platform = response.get("platform").toString();
+        String browser = response.get("browser").toString();
+        String device = response.get("device").toString();
+        assertEquals(platform, platforms, userAgent +" platform is wrong");
+        assertEquals(browser, browsers, userAgent + " browser is wrong");
+        assertEquals(device, devices, userAgent + " device is wrong");
+    }
+}
