257b0d0e08b417792edca9808841df4053992fff
lnoskov <k.noskov.kazanexpress@gmail.com>
2023-05-31 23:48:00 +0300

    Ex14

---
+++ src/test/java/lib/Assertions.java
@@ -0,0 +1,12 @@
+package lib;
+
+import io.restassured.response.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Assertions {
+    public static void asserJsonByName(Response Response, String name, int expectedValue) {
+        int value = Response.jsonPath().getInt(name);
+        assertEquals(expectedValue, value, "JSON value is not equal to expected value");
+    }
+}
